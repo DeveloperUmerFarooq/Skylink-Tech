82908e815a1b9a77b10279560530d9acff2babb5
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2024-12-18 16:04:33 +0500

    minor changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,49 @@
-var carouselElement = document.getElementById('carouselExample');
-var carousel = new bootstrap.Carousel(carouselElement, {
-    interval: 2000,  
-    ride: 'carousel' 
-});
+$(document).ready(function () {
+    var owl=$('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      center:true,
+      autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 2
+        },
+      }
+    });
+    var tech=$('.tech-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        center:true,
+        autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 2
+          },
+          1000: {
+            items: 2
+          },
+        }
+      });
+    $('.prev').click(function() {
+      owl.trigger('prev.owl.carousel');
+    });
+    $('.next').click(function() {
+      owl.trigger('next.owl.carousel');
+    });
+  });
+  
